7bb5579664f0f38cedacc463fa8da4e56433ae97
Sarah Verbelen <sarah.verbelen@student.ehb.be>
2019-05-20 13:43:40 +0200

    uitklappen gefixt

--- www/js/overzichtkaartjes.js
+++ www/js/overzichtkaartjes.js
@@ -1,35 +1,28 @@
 $(document).ready(function () {
     $(".alleMethodes").hide();
 
-    var arrowUp = false;
-
-    $(".kaartje").on("click", function () {
-        //anderen toeklappen
-        $(".alleMethodes").slideUp();
-        $(".fa-angle-down").css({"transform": "rotate(45deg)"}).css({"-webkit-transform": "rotate(45deg)"});
-        arrowUp = false;
-        
+    $(".kaartje").on("click", function () {    
         //deze uitklappen
         $(this).children(".alleMethodes").slideToggle();
-
-        if (arrowUp) {
-            $(this).children(".fa-angle-down").css({ //pijltje naar beneden draaien
-                "transform": "rotate(45deg)"
-            }).css({
-                "-webkit-transform": "rotate(45deg)"
-            }).css({
-                "margin-top": "1vh"
-            });
-        } else {
-            $(this).children(".fa-angle-down").css({ //pijltje naar boven draaien
+        if ($(this).children(".fa-angle-down").css("transform") == "matrix(0.707107, 0.707107, -0.707107, 0.707107, 0, 0)" ) {
+            //checkt naar welke kant het pijltje staat en draait het naar de andere kant
+            $(this).children(".fa-angle-down").css({ 
                 "transform": "rotate(-135deg)"
             }).css({
                 "-webkit-transform": "rotate(-135deg)"
             }).css({
                 "margin-top": "1.9vh"
             });
+        } else {
+            $(this).children(".fa-angle-down").css({ 
+                "transform": "rotate(45deg)"
+            }).css({
+                "-webkit-transform": "rotate(45deg)"
+            }).css({
+                "margin-top": "1vh"
+            });
         }
-        
-        arrowUp = !arrowUp;
+         
+
     })
 });
