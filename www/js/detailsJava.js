f2145f106195b4e516ee6b156a699faf54a92af8
cyril.passeleur <cyril.passeleur@student.ehb.be>
2019-05-20 14:43:44 +0200

    quazy javascript af.. enkel nog data afhalen

--- www/js/detailsJava.js
+++ www/js/detailsJava.js
@@ -1,12 +1,47 @@
-$(document).ready(function(){
-    
+$(document).ready(function () { 
     console.log("ready");
     $(".tuto").hide();
+    $(".content").hide();
     
+    var number = 1;
+    var ad = 2;
+    $(".ad h1").text("D" + ad);
+
+    //vraagteken//
     $("h1 img").click(function () {
-        
-        $(".tuto").fadeToggle().css("display", "inline");   
+        $(this).parent().find(".tuto").fadeToggle().css("display", "inline");
+    });
+
+    //switchcheckbox//
+    $('.check').click(function () {
+        if ($(this).is(':checked')) {
+            $(".content").slideDown();
+        } else {
+            $(".content").slideUp();
+        }
+    });
+
+    //textareaautoresize//
+ var $textareas = $('textarea');
+
+  $textareas.on('input', autosize);
+
+  function autosize() {
+    var $this = $(this);
+
+    $this
+      .css({height: 'auto'})
+      .css({height: $this.prop('scrollHeight')});
+  }
+
+    //doelstelling toevoegen//
+    $(".toev").click(function () {
+        number = number + 1;
+        ad = ad + 1;
+        $(".ad h1").text("D" + ad);
+        $(".contentToev").append('<div class="ADD"><h1>D' + number +':</h1><textarea rows="1" placeholder="lorem ipsum dolor sit amet" style="resize: none;"></textarea></div>');
+        return false;
         
     });
-    
-}); 
+
+});
