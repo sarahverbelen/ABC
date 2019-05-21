3a2c9b4ff40782a9cf144bd6b9db9f165b0bafcc
Sarah Verbelen <sarah.verbelen@student.ehb.be>
2019-05-21 16:04:00 +0200

    verderwerken dynamisch gedisplayed

--- www/js/storyboardLaden.js
+++ www/js/storyboardLaden.js
@@ -2,8 +2,31 @@ $(document).ready(function () {
 
     var storyboardsJson = window.localStorage.getItem("storyboards");
     var storyboards = JSON.parse(storyboardsJson);
-
     
+    $(".StoryboardInfo").remove();
+
+    function displayStoryboard(storyboard){
+        var storyboardHTML = $("<div>", {"class": "StoryboardInfo"})
+        .append(
+            $("<a>", {"href": "storyboard.html"})
+            .append(
+                $("<h6>").text(storyboard.naam)
+            )
+            .append(
+                $("<p>", {"class": "fontRegular extraInfo"}).text("Deeltraject: " + storyboard.deeltraject)
+            )
+            .append(
+                $("<p>", {"class": "fontRegular extraInfo"}).text("Semester: " + storyboard.semester)
+            )
+        );
+        
+        $(".content").append(storyboardHTML);
+    }
+    
+    
+    for(var i = 0; i < storyboards.length; i++){
+        displayStoryboard(storyboards[i]);
+    }
     
     
     $(".StoryboardInfo a").on("click", function (e) {
