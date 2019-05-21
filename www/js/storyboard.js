8c703947483106e3ba846feedd2b2e81ca6b47c6
Sarah Verbelen <sarah.verbelen@student.ehb.be>
2019-05-21 14:28:16 +0200

    fixed kaartje toevoegen

--- www/js/storyboard.js
+++ www/js/storyboard.js
@@ -1,51 +1,51 @@
 $(document).ready(function () {
-    
+
     //PLUSKNOP OPEN/TOE-KLAPPEN
     $(".plusKaartjes").hide();
 
     $("#plus").on("click", function () {
         $(".plusKaartjes").slideToggle();
     });
-    
+
     //DOELSTELLINGEN OPEN/TOE-KLAPPEN  
     $(".doelstellingenInhoudOpen").hide();
-    
-    $("#doelstellingen, #doelstellingDropdown").on("click", function(){
-       $(".doelstellingenInhoudOpen").slideToggle(); 
+
+    $("#doelstellingen, #doelstellingDropdown").on("click", function () {
+        $(".doelstellingenInhoudOpen").slideToggle();
     });
-    
+
     //DISPLAY KAARTJE - FUNCTIE
-    function displayKaartje(kaartje){
+    function displayKaartje(kaartje) {
         var kaartjeHTML = $("<div>", {
-            "class": "kaartje " + kaartje.kleur
-        })
-        .append(
-            $("<div>", {
-                "class": "triangle" + kaartje.kleur + " triangle"
+                "class": "kaartje " + kaartje.kleur
             })
-        )
-        .append(
-            $("<a>", {
-                "class": "editKaartje",
-                "href": "#"
-            }).text("edit kaartje")
-        )
-        .append(
-            $("<h2>", {
-                "class": "storyboardKaart"
-            }).text(kaartje.activiteit.charAt(0).toUpperCase() + kaartje.activiteit.slice(1))
-        )
-        .append(
-            $("<h3>", {
-                "class": "fontRegular storyboardKaart"
-            }).text(kaartje.methodiek)
-        )
-        .append(
-            $("<p>", {
-                "class": "storyboardKaart"
-            }).text(kaartje.nota)
-        );
-        
+            .append(
+                $("<div>", {
+                    "class": "triangle" + kaartje.kleur + " triangle"
+                })
+            )
+            .append(
+                $("<a>", {
+                    "class": "editKaartje",
+                    "href": "#"
+                }).text("edit kaartje")
+            )
+            .append(
+                $("<h2>", {
+                    "class": "storyboardKaart"
+                }).text(kaartje.activiteit.charAt(0).toUpperCase() + kaartje.activiteit.slice(1))
+            )
+            .append(
+                $("<h3>", {
+                    "class": "fontRegular storyboardKaart"
+                }).text(kaartje.methodiek)
+            )
+            .append(
+                $("<p>", {
+                    "class": "storyboardKaart"
+                }).text(kaartje.nota)
+            );
+
         $(".kaartjes").append(kaartjeHTML);
     }
 
@@ -55,57 +55,62 @@ $(document).ready(function () {
     if (nieuwStoryboard == "true") {
         $(".kaartjes .kaartje").remove();
         window.localStorage.setItem("nieuwStoryboard", false);
-        
+
         var kaartjes = [];
-        
+
         var lesfase1 = new Lesfase("lesfase 1", "", "", kaartjes)
-        
+
         var lesfasen = [lesfase1];
 
         var storyboard = new Storyboard("nieuw Storyboard", lesfasen, "", "", "", "")
 
-        $(".plusKaartjes a").on("click", function (e) {
-            e.preventDefault();
-            var kleur = $(this).attr("class");
-            var activiteit;
+    
 
-            switch (kleur) {
-                case "plusOranje":
-                    kleur = "oranje";
-                    break;
-                case "plusGeel":
-                    kleur = "geel";
-                    break;
-                case "plusPaars":
-                    kleur = "paars";
-                    break;
-                case "plusGroen":
-                    kleur = "groen";
-                    break;
-                case "plusAqua":
-                    kleur = "aqua";
-                    break;
-                case "plusRoze":
-                    kleur = "roze";
-                    break;
-            }
+    $(".plusKaartjes a").on("click", function (e) {
+        e.preventDefault();
+        
+        var kleur, activiteit;
+        kleur = $(this).attr("class");
 
-            $.ajax({
-                "url": "../json/kaartjes.json"
-            }).done(function(data){
-                activiteit = data[kleur].activiteit; 
-                
-                var kaartje = new Kaartje(kleur, "", "", activiteit);
-            
+        switch (kleur) {
+            case "plusOranje":
+                kleur = "oranje";
+                break;
+            case "plusGeel":
+                kleur = "geel";
+                break;
+            case "plusPaars":
+                kleur = "paars";
+                break;
+            case "plusGroen":
+                kleur = "groen";
+                break;
+            case "plusAqua":
+                kleur = "aqua";
+                break;
+            case "plusRoze":
+                kleur = "roze";
+                break;
+        }
+
+        $.ajax({
+            "url": "../json/kaartjes.json"
+        }).done(function (data) {
+            console.log(data);
+            activiteit = data[kleur].activiteit;
+
+            var kaartje = new Kaartje(kleur, "", "", activiteit);
+
             kaartjes.push(kaartje);
             displayKaartje(kaartje);
-            
+
             console.log(storyboard);
-            });
-            
+        }).error(function(een, twee, drie){
+            console.log(een);
+            console.log(twee);
+            console.log(drie);
         });
 
-    }
-
-
+    });
+};
 });
