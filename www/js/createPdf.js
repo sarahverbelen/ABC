a563d2b7a8f8127a9cecfec4c97831cf233ebd09
Axelle Vanden Eynde <axelle.vanden.eynde@student.ehb.be>
2019-05-23 21:56:10 +0200

    begin uitwerking pdf + treshold swipe

--- www/js/createPdf.js
+++ www/js/createPdf.js
@@ -1,11 +1,80 @@
-$(document).ready(function(){
-$("button").on("click",function() {
-    var test = {
+$(document).ready(function () {
+    $("button").on("click", function () {
+        //////////////////////////////
+
+        var docDefinition = {
+        pageSize: 'A4',
+        pageOrientation: 'landscape',
+        pageMargins: [40,20,20, 40],
         content: [
-        'test', 'test2', 'HelloWorld! :D'
-    ]
-    };
-    console.log('hi');
-    pdfMake.createPdf(test).download();
-});
+            {
+                style: 'custom',
+                table: {
+                    widths: ['*','*','*'],
+                    Heights: ['*','*','*','*'],
+                    body: [
+                        // rij 1
+                            [
+                                {
+                                colSpan: 3,
+                                border: [false, false, false, true],
+                                fillColor: '#eeeeee',
+                                alignment: 'center',
+                                text: 'storyboard titel', // HERE
+						},
+                                {
+                                    
+                                },{
+                                    
+                                },
+					],
+                        
+                    // rij 2
+                        [
+                            {border: [false, false, true, false],
+                            text: 'Lesfase', // HERE
+                            bold: true,
+                            alignment: 'center'},
+                            {border: [false, false, true, false],
+                            text: 'Lesfase', // HERE
+                            bold: true,
+                            alignment: 'center'},
+                            {border: [false, false, false, false],
+                            text: 'Lesfase', // HERE
+                            bold: true,
+                            alignment: 'center'},
+                        ],
+                    // rij 3
+                        [
+                            {border: [false, false, true, false],
+                            text: 'Doelstellingen: ', // HERE
+                            alignment: 'left'},
+                            {border: [false, false, true, false],
+                            text: 'Doelstellingen: ', // HERE
+                            alignment: 'left'},
+                            {border: [false, false, false, false],
+                            text: 'Doelstellingen: ', // HERE
+                            alignment: 'left'},
+                        ],
+                    // rij 4
+                        [
+                            {border: [false, false, true, false],
+                            text: 'Inhoud: ', // HERE
+                            alignment: 'left'},
+                            {border: [false, false, true, false],
+                            text: 'Inhoud: ', // HERE
+                            alignment: 'left'},
+                            {border: [false, false, false, false],
+                            text: 'Inhoud: ', // HERE
+                            alignment: 'left'},
+                        ],
+                    ]
+                    }
+            }   
+        ]
+        };
+        //////////////////////////////
+
+        pdfMake.createPdf(docDefinition).download();
+    });
 });
