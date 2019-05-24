7d0a1aafadd6707875202e5e468820fcd260eef9
Sarah Verbelen <sarah.verbelen@student.ehb.be>
2019-05-24 14:22:47 +0200

    small fix

--- www/js/nieuwStoryboard.js
+++ www/js/nieuwStoryboard.js
@@ -3,8 +3,6 @@ $(document).ready(function(){
     $(".maakStoryboard").on("click", function(e){
         e.preventDefault();
         
-        console.log("zegiheofihzfhi");
-        
         localStorage.setItem("HuidigStoryboard", "-1");
         
         window.location = $(this).attr("href");
